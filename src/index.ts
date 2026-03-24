7e19f81fb2c0f70edc21c95eedea170d68a81929
sakruhnab1 <aruiskyhappy2@outlook.com>
2026-03-24 20:24:32 +0800

    feat: read file capability

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,11 @@
 #!/usr/bin/env node
-
-import { wrapAxiosWithPayment, x402Client } from "@x402/axios";
-import { ExactEvmScheme, toClientEvmSigner } from "@x402/evm";
-import { privateKeyToAccount } from "viem/accounts";
-import { createPublicClient, http } from "viem";
-import { baseSepolia } from "viem/chains";
 import dotenv from "dotenv";
-import axios from 'axios'
-import { Command, Option } from 'commander';
-import { open, writeSync } from 'node:fs';
 import OpenAI from 'openai';
-import { Sandbox } from 'e2b';
 import * as readline from "readline";
-import { resolve } from "node:dns";
 import { readFile } from "node:fs/promises";
+import { execSync } from "node:child_process";
+import { Tracing } from "node:trace_events";
+
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -28,23 +20,6 @@ const openai = new OpenAI({
 })
 
 const toolDefination: OpenAI.Chat.Completions.ChatCompletionTool[] = [
-  {
-    "type": "function",
-    "function": {
-      "name": "bash",
-      "description": "Run a bash command in a Linux terminal",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "command": {
-            type: "string",
-            description: "Run a bash command"
-          },
-        },
-        required: ["command"]
-      }
-    }
-  },
   {
     "type": "function",
     "function": {
@@ -64,17 +39,18 @@ const toolDefination: OpenAI.Chat.Completions.ChatCompletionTool[] = [
   }
 ]
 
-const message: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = []
+const filetree = execSync(`ls ${process.cwd()}`).toString()
 
-const sandbox = await Sandbox.create({ timeoutMs: 5 * 60 * 1000 })
+const message: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+  {
+    role: 'system',
+    content: `You are a coding agent assistance. The file structure: ${filetree}`
+  }
+]
 
 while(true) {
   const prompt = await ask('> ')
   message.push({ role: 'user', content: prompt})
-  // const message: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{
-  //   "role": 'user',
-  //   "content": prompt,//'用 curl 获取 https://httpbin.org/ip 的返回内容，然后用 echo 把 IP 地址单独打印出来',
-  // }]
 
   while (true) {
     const completion = await openai.chat.completions.create({
@@ -93,17 +69,9 @@ while(true) {
           const fn = (call as any).function
           const args = JSON.parse(fn.arguments)
           const command = args.command
-          const result = await sandbox.commands.run(command)
-
-          console.log(result.stdout || result.stderr || 'no output')
-          message.push({
-            role: 'tool',
-            tool_call_id: call.id,
-            content: result.stdout || result.stderr || 'no output'
-          })
         } else if (call.function.name == 'read_file'){
           const args = JSON.parse(call.function.arguments)
-          const content = await read_file(args)
+          const content = await read_file(args.path)
           message.push({
             role: 'tool',
             tool_call_id: call.id,
@@ -112,7 +80,6 @@ while(true) {
         }
       }
     } else if (completion.choices[0].finish_reason === 'stop') {
-      sandbox.kill()
       break
     }
   }
@@ -127,55 +94,12 @@ function ask(prompt: string): Promise<string> {
 }
 
 async function read_file(file_path: string) {
-  const data = await readFile(file_path, { encoding: 'utf-8'});
+  console.log(file_path)
+  const data = await readFile(`${file_path}`, { encoding: 'utf-8'});
   console.log(data)
   return data
 }
 
-// const account = privateKeyToAccount(process.env.EVM_PRIVATE_KEY as `0x{string}`)
-// const publicClient = createPublicClient({ chain: baseSepolia, transport: http()})
-// const signer = toClientEvmSigner(account, publicClient)
-
-// const client = new x402Client()
-// client.register("eip155:*", new ExactEvmScheme(signer))
-
-// const api = wrapAxiosWithPayment(
-//   axios.create({ baseURL: "https://localhost:4021"}),
-//   client
-// )
-
-// const response = await api.post("/sandbox", )
-
-
-// const program = new Command()
-
-// program
-//  .version("1.0.0")
-//  .description("x402 Agent Skills Payment CLI")
-//  .option('-p, --prompt <value>', 'Your prompt passed to needed agent skill')
-
-// program.parse(process.argv)
-
-// const options = program.opts()
-
-// const account = privateKeyToAccount(process.env.EVM_PRIVATE_KEY as `0x${string}`);
-// const publicClient = createPublicClient({ chain: baseSepolia, transport: http() });
-// const signer = toClientEvmSigner(account, publicClient);
-
-// const client = new x402Client();
-// client.register("eip155:*", new ExactEvmScheme(signer))
-
-// const api = wrapAxiosWithPayment(
-//   axios.create({ baseURL: "http://localhost:4021", timeout: 300000 }),
-//   client
-// )
-
-// const str = options.prompt
-// writeSync(1, "正在发送支付请求...\n")
-// try {
-//   const response = await api.post("/skill", { str });
-//   writeSync(1, `Response: ${response.data.stdout}\n`);
-// } catch (e: any) {
-//   writeSync(2, `Error: ${JSON.stringify(e.response?.data || e.message)}\n`);
-//   process.exitCode = 1;
-// }
+async function write_file(file_path: string, content: string) {
+  
+}
